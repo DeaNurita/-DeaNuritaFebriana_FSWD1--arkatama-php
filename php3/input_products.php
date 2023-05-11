7154385f93137016275eecacb99e1334a6ccb4a6
DeaNurita <deanuritafebriana@gmail.com>
2023-05-11 09:52:26 +0700

    [ADD] Tugas 19

--- php3/input_products.php
+++ php3/input_products.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../php3/assets/css/style.css">
 </head>
 <body>
 
 <h1>Add Categories</h1>
-<button><a class="link" href="../php/read_categories.php">read_categories</a></button>
-<button><a class="link" href="../php/read_products.php">read_products</a></button>
+<button><a class="link" href="../php3/read_categories.php">read_categories</a></button>
+<button><a class="link" href="../php3/read_products.php">read_products</a></button>
 
 <form method="post" action="create.php">
   <label for="id">id:</label>
@@ -60,5 +60,10 @@
 
   <input type="submit" value="Submit">
 </form>
+
+<!-- Tutup koneksi -->
+<?php
+mysqli_close($conn);
+?>
 </body>
 </html>
